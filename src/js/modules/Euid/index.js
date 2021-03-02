6c17ce295855f1e0cab48b9643ca5e45047eb1a3
WonjunJang <wonjun84@naver.com>
2021-03-02 15:56:05 +0900

    feat: Complete assignment

--- src/js/modules/Euid/index.js
+++ src/js/modules/Euid/index.js
@@ -1,1 +1,7 @@
 /**@Euid 모듈 엔트리 파일 */
+import * as logger from './logger.js';
+import * as utils from './utils.js';
+import * as tester from './tester.js';
+
+
+export default { logger, utils, tester };
